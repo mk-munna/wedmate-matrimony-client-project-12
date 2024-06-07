2ba06e6b81d4c09236b3789e79d0764373e0e010
mk-munna <mkmunnaofficial@gmail.com>
2024-06-08 03:19:32 +0600

    fixed how it works section

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -13,7 +13,7 @@ const HowItWorks = () => {
     return (
         <div className='pt-24'>
             <div>
-                <h1 className='text-center text-heading text-5xl dark:text-heading2'>How It Works</h1>
+                <h1 className='text-center text-heading font-[500] mb-6 text-5xl dark:text-heading2'>How It Works</h1>
                 <div className='relative'>
                     <img className='absolute lg:w-[250px] w-[150px] rotating left-[50%] ' src="https://rn53themes.net/themes/matrimo/images/leaf/1.png" alt="" />
                     <img className='absolute lg:w-[250px] w-[150px] rotating2 right-[50%] ' src="https://rn53themes.net/themes/matrimo/images/leaf/1.png" alt="" />
@@ -30,8 +30,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#028391', color: '#fff' }}
                         icon={<FaHourglassStart />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Register</h3>
-                        <p className='text-Description dark:text-Description2'>
+                        <h3 className=" text-3xl lg:!font-light text-heading dark:text-heading2">Register</h3>
+                        <p className='  font-secondary !leading-[26px] text-Description dark:text-Description2 '>
                             Begin by creating an account on our platform. Fill out your profile details, including personal information and interests. This helps us match you with the most relevant profiles.
                         </p>
                     </VerticalTimelineElement>
@@ -44,8 +44,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#617A55', color: '#fff' }}
                         icon={<FaSearch />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Find Your Match</h3>
-                        <p className='text-Description dark:text-Description2'>
+                        <h3 className=" text-3xl font-light text-heading dark:text-heading2">Find Your Match</h3>
+                        <p className='text-Description  font-secondary !leading-[26px] dark:text-Description2'>
                             Use our algorithm to find potential matches based on your preferences. Browse profiles using various filters to discover someone who aligns with your values and interests.
                         </p>
                     </VerticalTimelineElement>
@@ -58,8 +58,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#AF8F6F', color: '#fff' }}
                         icon={<BsFillSendCheckFill />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Send Interest</h3>
-                        <p className='text-Description dark:text-Description2'>
+                        <h3 className=" text-3xl font-light text-heading dark:text-heading2">Send Interest</h3>
+                        <p className='text-Description  font-secondary !leading-[26px] dark:text-Description2'>
                             Express your interest in someone by sending a message. Break the ice and start a conversation with potential matches to show genuine interest and make a connection.
                         </p>
                     </VerticalTimelineElement>
@@ -72,8 +72,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#539165', color: '#fff' }}
                         icon={<FaUserGraduate />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Get Profile Information</h3>
-                        <p className='text-Description dark:text-Description2'>
+                        <h3 className=" text-3xl font-light  text-heading dark:text-heading2">Get Profile Information</h3>
+                        <p className='text-Description  font-secondary !leading-[26px] dark:text-Description2'>
                             Access detailed information about your match's profile to learn about their background, interests, and partner preferences. Understand if you both share common goals and values.
                         </p>
                     </VerticalTimelineElement>
@@ -86,8 +86,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#51829B', color: '#fff' }}
                         icon={<ImManWoman />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Start Meetups</h3>
-                        <p className='text-Description dark:text-Description2'>
+                        <h3 className=" text-3xl font-light text-heading dark:text-heading2">Start Meetups</h3>
+                        <p className='text-Description  font-secondary !leading-[26px] dark:text-Description2'>
                             Plan meetups in a safe environment to get to know each other better. Face-to-face interactions are essential for building a strong connection and understanding each other deeply.
                         </p>
                     </VerticalTimelineElement>
@@ -100,8 +100,8 @@ const HowItWorks = () => {
                         iconStyle={{ background: '#e77878', color: '#fff' }}
                         icon={<TbHeartHandshake />}
                     >
-                        <h3 className=" text-3xl text-heading dark:text-heading2">Getting Marriage</h3>
-                        <p className='text-Description  dark:text-Description2'>
+                        <h3 className=" text-3xl font-light text-heading dark:text-heading2">Getting Marriage</h3>
+                        <p className='text-Description  font-secondary !leading-[26px]  dark:text-Description2'>
                             As your relationship grows, consider marriage. Our platform provides tips and resources to help you prepare for a lifelong commitment. Celebrate your love story and start your happily ever after.
                         </p>
                     </VerticalTimelineElement>
