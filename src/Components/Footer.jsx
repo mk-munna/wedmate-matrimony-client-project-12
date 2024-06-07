54e6bbdc781f3a7a6f3a7909479ddfbe6d51b91b
mk-munna <mkmunnaofficial@gmail.com>
2024-06-08 04:26:31 +0600

    home page done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
 
     return (
         <div className='mt-12 relative'>
-            <hr />
+            <hr className='dark:border-gray-600' />
             <div className='my-20 px-10 lg:px-20 flex flex-col lg:flex-row gap-8'>
                 <motion.div
                     initial={{ opacity: 1, y: 120, x: 80 }}
@@ -36,20 +36,20 @@ const Footer = () => {
                 <div className="lg:w-[400px]">
                     <a href="#" title="" className="flex gap-2 items-center">
                         <img className='w-[40px]' src={logo} alt="" />
-                        <p className=' font-semibold text-3xl'>WedMate</p>
+                        <p className=' font-semibold text-heading dark:text-heading2 text-3xl'>WedMate</p>
                     </a>
-                    <p className='mt-4 text-xl'>Your Journey to Love Starts Here with WedMate</p>
-                    <button className='h-[55px] mt-6 w-[200px] rounded-full dynamic-button2 text-primary px-6 border-primary border hover:text-white py-3'><span className='absolute z-10 top-[15px] left-[40px]'>Register Now</span></button>
+                    <p className='mt-4 text-xl text-Description dark:text-Description2'>Your Journey to Love Starts Here with WedMate</p>
+                    <button className='h-[55px] mt-6 w-[200px] rounded-full dynamic-button2 text-primary dark:text-heading2 px-6 border-primary border hover:text-white py-3'><span className='absolute z-10 top-[15px] left-[40px]'>Register Now</span></button>
                 </div>
                 
                 <div className='flex gap-12 flex-col font-light md:flex-row text-lg'>
                     <div className='w-[340px]'>
-                        <h1 className='uppercase font-normal mb-8'>GET IN TOUCH</h1>
-                        <div className='space-y-3'>
-                            <p className='flex items-center gap-3'><SlLocationPin className='text-primary text-xl' />374 William S Canning Blvd, Fall River MA 2721, USA</p>
-                            <p className='flex items-center gap-3'><TbPhoneCall className='text-primary text-xl' />(+880)1893345368</p>
-                            <p className='flex items-center gap-3'><MdOutlineMarkEmailRead className='text-primary text-xl' />support@webmate.app</p>
-                            <p className='flex items-center gap-3'><BsClockHistory className='text-primary text-xl' />10:00 - 17:00</p>
+                        <h1 className='uppercase text-heading dark:text-heading2 font-normal mb-8'>GET IN TOUCH</h1>
+                        <div className='space-y-3 text-Description dark:text-Description2'>
+                            <p className='flex items-center gap-3'><SlLocationPin className='text-primary text-4xl' />374 William S Canning Blvd, Fall River MA 2721, USA</p>
+                            <p className='flex items-center gap-3'><TbPhoneCall className='text-primary text-2xl' />(+880)1893345368</p>
+                            <p className='flex items-center gap-3'><MdOutlineMarkEmailRead className='text-primary text-2xl' />support@webmate.app</p>
+                            <p className='flex items-center gap-3'><BsClockHistory className='text-primary text-2xl' />10:00 - 17:00</p>
                         </div>
                     </div>
                     
@@ -61,7 +61,7 @@ const Footer = () => {
                             className='absolute'>
                             <img className='w-[150px]' src="https://templates.hibotheme.com/wazo/default/assets/img/hero/hero-shape-6.png" alt="" />
                         </motion.div>
-                        <h1 className='uppercase font-normal mb-8'>Social media</h1>
+                        <h1 className='uppercase font-normal text-heading dark:text-heading2 mb-8'>Social media</h1>
                         <div className='flex gap-4'>
                             <a href="#!"><img src="https://rn53themes.net/themes/matrimo/images/social/1.png" alt="" loading="lazy"/></a>
                             <a href="#!"><img src="https://rn53themes.net/themes/matrimo/images/social/2.png" alt="" loading="lazy"/></a>
@@ -71,8 +71,8 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <hr />
-            <p className='font-light text-center my-4'>Copyright © 2024. <a href='#' className='underline cursor-pointer'>WedMate.com</a> All rights reserved.</p>
+            <hr className='dark:border-gray-600 '/>
+            <p className='font-light text-Description dark:text-Description2 text-center py-4'>Copyright © 2024. <a href='#' className='underline cursor-pointer'>WedMate.com</a> All rights reserved.</p>
         </div>
     );
 };
